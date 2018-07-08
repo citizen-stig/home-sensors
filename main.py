9b9ca4f0e859bbd443654d021754854784e3e3f0
Nikolay Golub <nikolay.v.golub@gmail.com>
2018-07-08 17:01:04 +0200

    Add actual code for sensors

--- main.py
+++ main.py
@@ -5,18 +5,41 @@ from carbon_client import CarbonClient
 from ssh import SSHTunnelWrapper
 
 
-def main():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+def read_sensors_data(config: configparser.ConfigParser) -> dict:
+    data = {}
+    prefix = config.get('carbon', 'prefix')
 
+    # Humidity and Temperature
+    humidity_and_temperature_pin = int(
+        config.get('sensors', 'humidity_and_temperature_pin'))
+    timestamp, humidity, temperature = sensors.get_humidity_and_temperature(
+        humidity_and_temperature_pin)
+    data[prefix + '.temperature'] = (timestamp, temperature)
+    data[prefix + '.humidity'] = (timestamp, humidity)
+
+    # Noise
+    sound_pin = int(
+        config.get('sensors', 'sound_pin'))
+    timestamp, noise = sensors.get_noise(sound_pin)
+    data[prefix + '.noise'] = (timestamp, noise)
+
+    # Dust
+    try:
+        timestamp, dust = sensors.get_dust()
+        data[prefix + '.dust'] = (timestamp, dust)
+    except ValueError:
+        pass
+
+    return data
+
+
+def send_to_carbon(config: configparser.ConfigParser, data: dict) -> None:
     carbon_host = config.get('carbon', 'host')
     carbon_port = int(config.get('carbon', 'port'))
-    prefix = config.get('carbon', 'prefix')
 
     ssh_host = config.get('ssh', 'host')
     ssh_user = config.get('ssh', 'user')
     ssh_key = config.get('ssh', 'pkey')
-    print('ssh -i {} {}@{}'.format(ssh_key, ssh_user, ssh_host))
 
     carbon_ssh_client = SSHTunnelWrapper(
         ssh_host=ssh_host,
@@ -27,24 +50,17 @@ def main():
         sender_class=CarbonClient,
     )
 
-    # Temperature and humidity
-    ht_pin = int(config.get('sensors', 'ht_pin'))
-    ht_timestamp, humidity, temperature = sensors.get_humidity_and_temperature(ht_pin)
-
-    carbon_ssh_client.send(prefix + '.temperature', ht_timestamp, temperature)
-    carbon_ssh_client.send(prefix + '.humidity', ht_timestamp, humidity)
-
-    # Noise
-
-    # Dust
+    for metric, item in data.items():
+        timestamp, value = item
+        carbon_ssh_client.send(metric, timestamp, value)
 
 
-    # for sensor_name, get_sensor_value in available_sensors:
-    #     timestamp, value = get_sensor_value()
-    #     metric = prefix + '.' + sensor_name
-    #     print(timestamp, metric, value)
-    #     # carbon_client.send(metric, timestamp,  value)
-    #     carbon_ssh_client.send(metric, timestamp, value)
+def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    data = read_sensors_data(config)
+    print(data)
 
 
 if __name__ == '__main__':
--- sensors.py
+++ sensors.py
@@ -1,14 +1,40 @@
-import Adafruit_DHT
 from datetime import datetime
-import random
 from typing import Tuple
+import atexit
+import time
+
+import Adafruit_DHT
+import grovepi
 
 
 def get_humidity_and_temperature(pin: int) -> Tuple[datetime, float, float]:
-    timestamp = datetime.utcnow()
+    timestamp = datetime.now()
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, pin)
     return timestamp, humidity, temperature
 
 
-def get_noise():
-    return random.randint(1, 30)
+def get_noise(pin: int) -> Tuple[datetime, int]:
+    grovepi.pinMode(pin, 'INPUT')
+    timestamp = datetime.now()
+    values = []
+    samples = 5
+    for i in range(samples):
+        value = grovepi.analogRead(pin)
+        values.append(value)
+        time.sleep(0.1)
+    avg_value = int(round(sum(values) / samples))
+    return timestamp, avg_value
+
+
+def get_dust() -> Tuple[datetime, int]:
+    atexit.register(grovepi.dust_sensor_dis)
+    grovepi.dust_sensor_en()
+    attempts = 100
+    for _ in range(attempts):
+        timestamp = datetime.now()
+        new_val, low_pulse_occupancy = grovepi.dustSensorRead()
+        if new_val:
+            return timestamp, low_pulse_occupancy
+        time.sleep(3)
+    else:
+        raise ValueError('Cannot read values from dust sensor')
